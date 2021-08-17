820e1405bf458bbca7a10a568c4c585e6aa34f56
zen-rox93 <84250823+zen-rox93@users.noreply.github.com>
2021-08-17 17:54:57 -0500

    feat: agregue el archivo variables.py

---
+++ semana1/Dia1/01-variables.py
@@ -0,0 +1,23 @@
+# holi
+
+nombre = "renzo"
+
+apellido = "estrada"
+
+direccion = "calle o' dd " 
+
+texto = """ hola maigos
+
+como estan  GG
+ggg"""
+
+texto2 = '''holas amigos:
+ya es break
+'''
+
+#variables numericas
+año = 2021
+estatuta = 1.92
+
+print (type (nombre))
+print(type(año))
