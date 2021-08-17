20d453e31338e4e2f2b5431d96ae797d96483971
zen-rox93 <84250823+zen-rox93@users.noreply.github.com>
2021-08-16 21:23:02 -0500

    feat:agregue el archivo variables.py

---
+++ Dia1/02-impresion-pantalla.py
@@ -0,0 +1,14 @@
+mes = "agosto"
+print("el mes es:" , mes)
+
+dia= 16
+
+print("el dia", dia, "del mes", mes)
+
+alumnos = 23
+grupo = 9
+ 
+print("los alumnos son {} y son del grupo {} y todoso estan atentos".format('eduardo', 10.5))
+
+#falta algunas lineas xD mira el repo del profe
+
