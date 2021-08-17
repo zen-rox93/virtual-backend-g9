bf6ea7f5a9ec7f5e23ac41ffe346e3da3290c3f3
zen-rox93 <84250823+zen-rox93@users.noreply.github.com>
2021-08-17 17:59:16 -0500

    feat: agregue el archivo impresion-pantalla.py

---
+++ semana1/Dia1/02-impresion-pantalla.py
@@ -0,0 +1,16 @@
+mes = "agosto"
+print("el mes es:" , mes)
+
+dia= 16
+
+print("el dia", dia, "del mes", mes)
+
+alumnos = 23
+grupo = 9
+ 
+print("los alunos son {} y son del grupo {} y todoso estan atentos".format(alumnos, grupo))
+
+print("los alumnos son {1} y son del grupo {0} y todos estan atentos".format(grupo, alumnos))
+
+print(f"los alumnos son {alumnos}")
+
