3d0f0048d6aede617e71315a4dbdf10491cad521
zen-rox93 <84250823+zen-rox93@users.noreply.github.com>
2021-08-28 13:28:26 -0500

    chore: cambios

--- Dia5/CAUSULA_GROUP_BY.sql
+++ Dia5/CAUSULA_GROUP_BY.sql
@@ -43,137 +43,30 @@ order by total desc, nombre asc;
 -- [GROUP BY AGRUPAMIENTOS]
 -- [ORDER BY COL ASC | DESC];
 
+use calificaciones;
 -- TRAER
 -- NOMBRE_ALUMNO | APELLIDO_ALUMNO| NOMBRE_CURSO | ESTADO_CURSO
+SELECT alumnos.nombre as NombreAlumno, alumnos.apellido as ApellidoAlumno,
+		cursos.nombre as NombreCurso, cursos.estado as EstadoCurso
+        
+FROM ALUMNOS
+INNER JOIN ALUMNOS_CURSOS ON alumnos.id = alumnos_cursos.id_alumno
+INNER JOIN cursos ON cursos.id = alumnos_cursos.id_curso;
+
+-- SABER CUANTOS ALUMNOS ESTAN INSCRITOS EN CADA CURSO
+
+select * from cursos;
+
+-- que este ordenado de mas a menos alumnos inscritos y si es el mismo numero
+-- de alumnos inscritos que me de los cursos ordenados alfabeticamente
+-- CURSO_NOMBRE | ALUMNOS_INSCRITOS
+SELECT count(id_alumno) as ALUMNOS_INSCRITOS, nombre as CURSO_NOMBRE
+from alumnos_cursos INNER JOIN cursos ON alumnos_cursos.id_curso = cursos.id
+group by nombre
+order by 1 desc, 2 asc;
+-- para el order by se puede poner los siguientes valores en relacion alos nombres
+-- de las columnas
+-- nombre original | alias | numerode la columna empezando en 1 
 
 
 
-                    
-CREATE DATABASE CALIFICACIONES;
-USE CALIFICACIONES;
-
-INSERT INTO ALUMNOS (nombre, apellido, correo) VALUES ('Matthew', 'Ramirez', 'khouston@gmail.com');
-INSERT INTO ALUMNOS (nombre, apellido, correo) VALUES ('Jeremy', 'Briggs', 'dawn66@thompson.com');
-INSERT INTO ALUMNOS (nombre, apellido, correo) VALUES ('Crystal', 'Gomez', 'laurenford@gmail.com');
-INSERT INTO ALUMNOS (nombre, apellido, correo) VALUES ('Paula', 'Griffin', 'millerdana@anderson-davis.org');
-INSERT INTO ALUMNOS (nombre, apellido, correo) VALUES ('Charles', 'Hart', 'gonzalezmaria@hotmail.com');
-INSERT INTO ALUMNOS (nombre, apellido, correo) VALUES ('Crystal', 'Adams', 'greerdaniel@yahoo.com');
-INSERT INTO ALUMNOS (nombre, apellido, correo) VALUES ('Zachary', 'Jensen', 'nancy85@smith.com');
-INSERT INTO ALUMNOS (nombre, apellido, correo) VALUES ('Alyssa', 'Perez', 'michaelflores@flores-king.com');
-INSERT INTO ALUMNOS (nombre, apellido, correo) VALUES ('Mark', 'Huynh', 'daniel40@hotmail.com');
-INSERT INTO ALUMNOS (nombre, apellido, correo) VALUES ('Kyle', 'Cisneros', 'lorimoore@hotmail.com');
-INSERT INTO ALUMNOS (nombre, apellido, correo) VALUES ('Debra', 'Edwards', 'jeremygarrett@maxwell.org');
-INSERT INTO ALUMNOS (nombre, apellido, correo) VALUES ('Rachel', 'Delgado', 'susan98@woods.com');
-INSERT INTO ALUMNOS (nombre, apellido, correo) VALUES ('Patricia', 'Oconnor', 'loretta34@sandoval.info');
-INSERT INTO ALUMNOS (nombre, apellido, correo) VALUES ('Matthew', 'Klein', 'jeffreynichols@hotmail.com');
-INSERT INTO ALUMNOS (nombre, apellido, correo) VALUES ('Daniel', 'Young', 'kanestephen@hotmail.com');
-INSERT INTO ALUMNOS (nombre, apellido, correo) VALUES ('Lauren', 'Thornton', 'gibbsjonathan@yahoo.com');
-INSERT INTO ALUMNOS (nombre, apellido, correo) VALUES ('Brianna', 'Shah', 'stevensonsean@colon.net');
-INSERT INTO ALUMNOS (nombre, apellido, correo) VALUES ('Lori', 'Shea', 'robertmartinez@hotmail.com');
-INSERT INTO ALUMNOS (nombre, apellido, correo) VALUES ('David', 'Crane', 'thomasstuart@horton-bowen.com');
-INSERT INTO ALUMNOS (nombre, apellido, correo) VALUES ('David', 'Williams', 'biancacarson@phillips.com');
-INSERT INTO ALUMNOS (nombre, apellido, correo) VALUES ('Lauren', 'Buck', 'joshua94@morrow.com');
-INSERT INTO ALUMNOS (nombre, apellido, correo) VALUES ('Jessica', 'Hawkins', 'moorepaige@yahoo.com');
-INSERT INTO ALUMNOS (nombre, apellido, correo) VALUES ('Justin', 'Mendoza', 'allenkevin@hotmail.com');
-INSERT INTO ALUMNOS (nombre, apellido, correo) VALUES ('Jesus', 'Meyers', 'garciamegan@gould.com');
-INSERT INTO ALUMNOS (nombre, apellido, correo) VALUES ('Michael', 'Weeks', 'jessica83@gmail.com');
-INSERT INTO ALUMNOS (nombre, apellido, correo) VALUES ('Christina', 'Harris', 'bcole@gmail.com');
-INSERT INTO ALUMNOS (nombre, apellido, correo) VALUES ('Ashlee', 'Smith', 'kevin08@hotmail.com');
-INSERT INTO ALUMNOS (nombre, apellido, correo) VALUES ('Brittney', 'Montoya', 'mfarley@hotmail.com');
-INSERT INTO ALUMNOS (nombre, apellido, correo) VALUES ('Kayla', 'Gallagher', 'whenson@roberts.com');
-INSERT INTO ALUMNOS (nombre, apellido, correo) VALUES ('Patrick', 'Torres', 'cassandra68@gomez-golden.com');
-INSERT INTO ALUMNOS (nombre, apellido, correo) VALUES ('Mark', 'Gonzalez', 'jcampbell@stewart-murray.info');
-INSERT INTO ALUMNOS (nombre, apellido, correo) VALUES ('Jonathan', 'Acosta', 'ian23@gmail.com');
-INSERT INTO ALUMNOS (nombre, apellido, correo) VALUES ('John', 'Mosley', 'mccarthyangela@lee.com');
-INSERT INTO ALUMNOS (nombre, apellido, correo) VALUES ('Mark', 'Cain', 'ashleymartin@pena-davis.net');
-INSERT INTO ALUMNOS (nombre, apellido, correo) VALUES ('Ryan', 'Hill', 'dixonreginald@flynn-le.com');
-INSERT INTO ALUMNOS (nombre, apellido, correo) VALUES ('Brad', 'Holmes', 'vhall@yahoo.com');
-INSERT INTO ALUMNOS (nombre, apellido, correo) VALUES ('Nathaniel', 'Townsend', 'kathleen40@ramirez.net');
-INSERT INTO ALUMNOS (nombre, apellido, correo) VALUES ('Sandra', 'Wagner', 'cruzphilip@hotmail.com');
-INSERT INTO ALUMNOS (nombre, apellido, correo) VALUES ('Crystal', 'Miller', 'kirstenstokes@olson.com');
-INSERT INTO ALUMNOS (nombre, apellido, correo) VALUES ('Casey', 'White', 'tyler63@mason.org');
-INSERT INTO ALUMNOS (nombre, apellido, correo) VALUES ('Sarah', 'Lawson', 'kristinjones@hotmail.com');
-INSERT INTO ALUMNOS (nombre, apellido, correo) VALUES ('Melissa', 'Hernandez', 'austinellis@hotmail.com');
-INSERT INTO ALUMNOS (nombre, apellido, correo) VALUES ('Robert', 'Palmer', 'yrodriguez@gmail.com');
-INSERT INTO ALUMNOS (nombre, apellido, correo) VALUES ('Anne', 'Chen', 'zmueller@yahoo.com');
-INSERT INTO ALUMNOS (nombre, apellido, correo) VALUES ('Michael', 'Cardenas', 'gmendoza@gmail.com');
-INSERT INTO ALUMNOS (nombre, apellido, correo) VALUES ('Michelle', 'Frost', 'jennifer91@gmail.com');
-INSERT INTO ALUMNOS (nombre, apellido, correo) VALUES ('Alyssa', 'Burgess', 'urodriguez@gmail.com');
-INSERT INTO ALUMNOS (nombre, apellido, correo) VALUES ('Jessica', 'Golden', 'washingtondavid@hotmail.com');
-INSERT INTO ALUMNOS (nombre, apellido, correo) VALUES ('Christine', 'Wilson', 'hurstjeremiah@hotmail.com');
-INSERT INTO ALUMNOS (nombre, apellido, correo) VALUES ('Matthew', 'Thompson', 'walkertaylor@yahoo.com');
-INSERT INTO ALUMNOS_CURSOS (ID_ALUMNO, ID_CURSO) VALUES (25, 4);
-INSERT INTO ALUMNOS_CURSOS (ID_ALUMNO, ID_CURSO) VALUES (32, 2);
-INSERT INTO ALUMNOS_CURSOS (ID_ALUMNO, ID_CURSO) VALUES (6, 3);
-INSERT INTO ALUMNOS_CURSOS (ID_ALUMNO, ID_CURSO) VALUES (50, 1);
-INSERT INTO ALUMNOS_CURSOS (ID_ALUMNO, ID_CURSO) VALUES (2, 4);
-INSERT INTO ALUMNOS_CURSOS (ID_ALUMNO, ID_CURSO) VALUES (30, 3);
-INSERT INTO ALUMNOS_CURSOS (ID_ALUMNO, ID_CURSO) VALUES (7, 4);
-INSERT INTO ALUMNOS_CURSOS (ID_ALUMNO, ID_CURSO) VALUES (40, 4);
-INSERT INTO ALUMNOS_CURSOS (ID_ALUMNO, ID_CURSO) VALUES (41, 5);
-INSERT INTO ALUMNOS_CURSOS (ID_ALUMNO, ID_CURSO) VALUES (41, 5);
-INSERT INTO ALUMNOS_CURSOS (ID_ALUMNO, ID_CURSO) VALUES (43, 4);
-INSERT INTO ALUMNOS_CURSOS (ID_ALUMNO, ID_CURSO) VALUES (43, 5);
-INSERT INTO ALUMNOS_CURSOS (ID_ALUMNO, ID_CURSO) VALUES (2, 1);
-INSERT INTO ALUMNOS_CURSOS (ID_ALUMNO, ID_CURSO) VALUES (35, 1);
-INSERT INTO ALUMNOS_CURSOS (ID_ALUMNO, ID_CURSO) VALUES (28, 5);
-INSERT INTO ALUMNOS_CURSOS (ID_ALUMNO, ID_CURSO) VALUES (7, 1);
-INSERT INTO ALUMNOS_CURSOS (ID_ALUMNO, ID_CURSO) VALUES (6, 2);
-INSERT INTO ALUMNOS_CURSOS (ID_ALUMNO, ID_CURSO) VALUES (39, 2);
-INSERT INTO ALUMNOS_CURSOS (ID_ALUMNO, ID_CURSO) VALUES (30, 1);
-INSERT INTO ALUMNOS_CURSOS (ID_ALUMNO, ID_CURSO) VALUES (3, 5);
-INSERT INTO ALUMNOS_CURSOS (ID_ALUMNO, ID_CURSO) VALUES (38, 2);
-INSERT INTO ALUMNOS_CURSOS (ID_ALUMNO, ID_CURSO) VALUES (50, 4);
-INSERT INTO ALUMNOS_CURSOS (ID_ALUMNO, ID_CURSO) VALUES (20, 1);
-INSERT INTO ALUMNOS_CURSOS (ID_ALUMNO, ID_CURSO) VALUES (2, 4);
-INSERT INTO ALUMNOS_CURSOS (ID_ALUMNO, ID_CURSO) VALUES (7, 5);
-INSERT INTO ALUMNOS_CURSOS (ID_ALUMNO, ID_CURSO) VALUES (42, 1);
-INSERT INTO ALUMNOS_CURSOS (ID_ALUMNO, ID_CURSO) VALUES (47, 3);
-INSERT INTO ALUMNOS_CURSOS (ID_ALUMNO, ID_CURSO) VALUES (26, 5);
-INSERT INTO ALUMNOS_CURSOS (ID_ALUMNO, ID_CURSO) VALUES (23, 5);
-INSERT INTO ALUMNOS_CURSOS (ID_ALUMNO, ID_CURSO) VALUES (15, 3);
-INSERT INTO ALUMNOS_CURSOS (ID_ALUMNO, ID_CURSO) VALUES (42, 3);
-INSERT INTO ALUMNOS_CURSOS (ID_ALUMNO, ID_CURSO) VALUES (22, 3);
-INSERT INTO ALUMNOS_CURSOS (ID_ALUMNO, ID_CURSO) VALUES (32, 2);
-INSERT INTO ALUMNOS_CURSOS (ID_ALUMNO, ID_CURSO) VALUES (15, 3);
-INSERT INTO ALUMNOS_CURSOS (ID_ALUMNO, ID_CURSO) VALUES (6, 1);
-INSERT INTO ALUMNOS_CURSOS (ID_ALUMNO, ID_CURSO) VALUES (29, 3);
-INSERT INTO ALUMNOS_CURSOS (ID_ALUMNO, ID_CURSO) VALUES (33, 5);
-INSERT INTO ALUMNOS_CURSOS (ID_ALUMNO, ID_CURSO) VALUES (29, 5);
-INSERT INTO ALUMNOS_CURSOS (ID_ALUMNO, ID_CURSO) VALUES (31, 4);
-INSERT INTO ALUMNOS_CURSOS (ID_ALUMNO, ID_CURSO) VALUES (42, 4);
-INSERT INTO ALUMNOS_CURSOS (ID_ALUMNO, ID_CURSO) VALUES (41, 2);
-INSERT INTO ALUMNOS_CURSOS (ID_ALUMNO, ID_CURSO) VALUES (13, 5);
-INSERT INTO ALUMNOS_CURSOS (ID_ALUMNO, ID_CURSO) VALUES (9, 1);
-INSERT INTO ALUMNOS_CURSOS (ID_ALUMNO, ID_CURSO) VALUES (24, 1);
-INSERT INTO ALUMNOS_CURSOS (ID_ALUMNO, ID_CURSO) VALUES (23, 3);
-INSERT INTO ALUMNOS_CURSOS (ID_ALUMNO, ID_CURSO) VALUES (38, 2);
-INSERT INTO ALUMNOS_CURSOS (ID_ALUMNO, ID_CURSO) VALUES (22, 3);
-INSERT INTO ALUMNOS_CURSOS (ID_ALUMNO, ID_CURSO) VALUES (31, 3);
-INSERT INTO ALUMNOS_CURSOS (ID_ALUMNO, ID_CURSO) VALUES (7, 2);
-INSERT INTO ALUMNOS_CURSOS (ID_ALUMNO, ID_CURSO) VALUES (15, 3);
-INSERT INTO ALUMNOS_CURSOS (ID_ALUMNO, ID_CURSO) VALUES (17, 2);
-INSERT INTO ALUMNOS_CURSOS (ID_ALUMNO, ID_CURSO) VALUES (40, 4);
-INSERT INTO ALUMNOS_CURSOS (ID_ALUMNO, ID_CURSO) VALUES (30, 5);
-INSERT INTO ALUMNOS_CURSOS (ID_ALUMNO, ID_CURSO) VALUES (35, 2);
-INSERT INTO ALUMNOS_CURSOS (ID_ALUMNO, ID_CURSO) VALUES (32, 3);
-INSERT INTO ALUMNOS_CURSOS (ID_ALUMNO, ID_CURSO) VALUES (37, 4);
-INSERT INTO ALUMNOS_CURSOS (ID_ALUMNO, ID_CURSO) VALUES (33, 4);
-INSERT INTO ALUMNOS_CURSOS (ID_ALUMNO, ID_CURSO) VALUES (30, 2);
-INSERT INTO ALUMNOS_CURSOS (ID_ALUMNO, ID_CURSO) VALUES (5, 2);
-INSERT INTO ALUMNOS_CURSOS (ID_ALUMNO, ID_CURSO) VALUES (2, 1);
-INSERT INTO ALUMNOS_CURSOS (ID_ALUMNO, ID_CURSO) VALUES (21, 4);
-INSERT INTO ALUMNOS_CURSOS (ID_ALUMNO, ID_CURSO) VALUES (13, 2);
-INSERT INTO ALUMNOS_CURSOS (ID_ALUMNO, ID_CURSO) VALUES (40, 1);
-INSERT INTO ALUMNOS_CURSOS (ID_ALUMNO, ID_CURSO) VALUES (12, 3);
-INSERT INTO ALUMNOS_CURSOS (ID_ALUMNO, ID_CURSO) VALUES (18, 1);
-INSERT INTO ALUMNOS_CURSOS (ID_ALUMNO, ID_CURSO) VALUES (15, 3);
-INSERT INTO ALUMNOS_CURSOS (ID_ALUMNO, ID_CURSO) VALUES (14, 4);
-INSERT INTO ALUMNOS_CURSOS (ID_ALUMNO, ID_CURSO) VALUES (20, 4);
-INSERT INTO ALUMNOS_CURSOS (ID_ALUMNO, ID_CURSO) VALUES (18, 1);
-INSERT INTO ALUMNOS_CURSOS (ID_ALUMNO, ID_CURSO) VALUES (44, 5);
-INSERT INTO ALUMNOS_CURSOS (ID_ALUMNO, ID_CURSO) VALUES (25, 1);
-INSERT INTO ALUMNOS_CURSOS (ID_ALUMNO, ID_CURSO) VALUES (50, 1);
-INSERT INTO ALUMNOS_CURSOS (ID_ALUMNO, ID_CURSO) VALUES (30, 5);
-INSERT INTO ALUMNOS_CURSOS (ID_ALUMNO, ID_CURSO) VALUES (41, 4);
-INSERT INTO ALUMNOS_CURSOS (ID_ALUMNO, ID_CURSO) VALUES (30, 5);
