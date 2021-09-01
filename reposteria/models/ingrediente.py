548df2b55aaa31ae56c1afe8b541bf21e0a85b24
zen-rox93 <84250823+zen-rox93@users.noreply.github.com>
2021-08-31 22:03:04 -0500

    chore: cambios

--- reposteria/models/ingrediente.py
+++ reposteria/models/ingrediente.py
@@ -9,4 +9,5 @@ class IngredienteModel(base_de_datos.Model):
     IngredienteId = Column(name='id', type_=types.Integer, primary_key=True, 
                             unique=True, autoincrement=True, nullable=False)
 
-ingredienteNombre = Column(name='nombre', type_=types.String(length=45))
+    ingredienteNombre = Column(name='nombre', type_=types.String(length=45))
+
