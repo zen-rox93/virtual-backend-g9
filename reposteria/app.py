3a4d146be68d3a6d77329a1a7861b44855bcb1e3
zen-rox93 <84250823+zen-rox93@users.noreply.github.com>
2021-08-31 20:13:08 -0500

    chore: cambios

--- reposteria/app.py
+++ reposteria/app.py
@@ -1,9 +1,18 @@
 from flask import Flask
 from conexion_bd import base_de_datos
+from models.ingrediente import IngredientesModel
+from models.receta import RecetaModel
+from models.preparacion import PreparacionModel
+from models.recetas_ingredientes import RecetaIngredienteModel
+from os import environ
+from dotenv import load_dotenv
+load_dotenv()
+
+print(environ.get('DATABASE_URI'))
 
 app = Flask(__name__)
 #                                        mysql://username: password@host/db_name
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:root@localhost/reposteria_flask'
+app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('DATABASE_URI')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 #inicia la conexion con la bd para darle las credenciales definidas en el app.config
 
