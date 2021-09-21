46d427d706cd61824c919aa117d03168b25ebaf3
zen-rox93 <84250823+zen-rox93@users.noreply.github.com>
2021-09-20 22:02:34 -0500

    avance modelado

---
+++ libreria/gestion/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+#https://docs.djangoproject.com/en/3.2/ref/models/
+#https://docs.djangoproject.com/en/3.2/topics/db/models/
+
+
+class ProductoModel(models.Model):
+#tipos de datos del ORM => https://docs.djangoproject.com/en/3.2/ref/models/fields/
+# Parametros genericos de los tipos de datos =>https://docs.djangoproject.com/en/3.2/ref/models/fields/#field-options
+    productoId = models.AutoField(
+        primary_key=True, null=False, unique=True, db_column='id')
+    
+    productoNombre = models.CharField(
+        max_length=45, db_column='nombre', null=False)
+    
