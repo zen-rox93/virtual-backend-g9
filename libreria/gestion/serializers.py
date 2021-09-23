4948c1459020af2e126d3a8246d7cd2329042ea8
zen-rox93 <84250823+zen-rox93@users.noreply.github.com>
2021-09-22 22:22:04 -0500

    chore: avance del miercoles

---
+++ libreria/gestion/serializers.py
@@ -0,0 +1,16 @@
+from rest_framework import serializers
+from .models import ProductoModel
+
+class ProductoSerializer(serializers.ModelSerializer):
+    class Meta:
+        # esto vinculara el serializador con el modelo respectivo para jlar sus atributos y hacer validaciones correspondientes
+        model = ProductoModel
+        # indica que campos tengo q mostrar para deserealizacion
+        # fields = '__all__' =>  indicara que haremos uso de todas los atributos del modelo
+
+        fields='__all__'
+        # exclude = ['campo1','campo2'] => excluira tanto de la peticion como del retorno de elementos los atributos indicados
+        # exclude = ['productoId']
+
+        # no se puede utilizar los dos atributos al mismo tiempo, es decir, o usamos el exclude o usamos el fields 
+         
