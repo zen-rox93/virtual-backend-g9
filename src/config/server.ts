c6782ed1efa76ceefadfb4842c5f5597378cf49c
zen-rox93 <84250823+zen-rox93@users.noreply.github.com>
2021-10-12 22:09:17 -0500

    chore:cambios

---
+++ src/config/server.ts
@@ -0,0 +1,26 @@
+import express, {Express, json} from 'express';
+import conexion from "./sequelize";
+
+export class Server {
+    // private = no podra ser aaccedido desde fuera de la clase
+    // readonly = no podra ser modificado su valor afuera del constructor
+    private readonly app: Express;
+    private readonly puerto: number;
+    constructor(){
+        this.app = express();
+        this.puerto = 8000;
+    }
+    bodyParser(){
+        this.app.use(json());
+    }
+
+    public start(){
+        this.app.listen(this.puerto, async()=>{
+            console.log(
+                `Servidor corriendo exitosamente en el puerto ${this.puerto}`
+            );
+            await conexion.sync()
+            console.log("Base de datos conectada exitosamente")
+        });
+    }
+}
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import {Server} from "./config/server";
+
+const objServer = new Server();
+
+objServer.start();
+
